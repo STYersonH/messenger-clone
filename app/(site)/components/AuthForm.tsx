105697681c48def400bedf0037ad2ab48e37da7c
STYersonH <102554781+STYersonH@users.noreply.github.com>
2023-06-21 10:35:00 -0500

    AuthForm done

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,10 @@
 "use client"; // renderizar solo en el lado del cliente -> NEXT 13
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/input";
 import { useCallback, useState } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import { BsGithub, BsGoogle } from "react-icons/bs";
+import AuthSocialButton from "./AuthSocialButton";
 
 // Limitar los valores a elegir
 type Variant = "LOGIN" | "REGISTER";
@@ -55,7 +59,76 @@ const AuthForm = () => {
 	return (
 		<div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
 			<div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-				<form className="space-y-6" onSubmit={handleSubmit(onSubmit)}></form>
+				<form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+					{variant === "REGISTER" && (
+						<Input
+							id="name"
+							label="Name"
+							register={register}
+							errors={errors}
+							disabled={isLoading} // se deshabilita el input cuando se esta enviando
+						/>
+					)}
+					<Input
+						id="email"
+						label="Email adress"
+						type="email"
+						register={register}
+						errors={errors}
+						disabled={isLoading}
+					/>
+					<Input
+						id="password"
+						label="Password"
+						type="password"
+						register={register}
+						errors={errors}
+						disabled={isLoading}
+					/>
+					<div>
+						{/* Como el boton esta dentro del form, no necesitamos un onClick, se enviara por defecto */}
+						<Button disabled={isLoading} fullWidth type="submit">
+							{variant === "LOGIN" ? "Sign in" : "Register"}
+						</Button>
+					</div>
+				</form>
+
+				<div className="mt-6">
+					<div className="relative">
+						{/* haciendo el efecto de ----------- texto ----------- */}
+						<div className="absolute inset-0 flex items-center">
+							<div className="w-full border-t border-gray-300" />
+						</div>
+						<div className="relative flex justify-center text-sm">
+							<span className="bg-white px-2 text-gray-500">
+								Or continue with
+							</span>
+						</div>
+					</div>
+
+					{/* Registro con github y google */}
+					<div className="mt-6 flex gap-2">
+						<AuthSocialButton
+							icon={BsGithub}
+							onClick={() => socialAction("github")}
+						/>
+						<AuthSocialButton
+							icon={BsGoogle}
+							onClick={() => socialAction("google")}
+						/>
+					</div>
+				</div>
+
+				<div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+					<div>
+						{variant === "LOGIN"
+							? "New to Messenger?"
+							: "Already have an account?"}
+					</div>
+					<div onClick={toggleVariant} className="underline cursor-pointer">
+						{variant === "LOGIN" ? "Create an account" : "Login"}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
